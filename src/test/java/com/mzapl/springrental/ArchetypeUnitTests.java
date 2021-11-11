e82e66656d687dffa7758da522a297138c2ea0dc
mzapl <mza@ovh.com>
2021-11-11 20:35:41 +0100

    moved @Test annotation to org.junit.jupiter.api.Test class. BeforeEach and AfterEach should work properly will all tests now.

--- src/test/java/com/mzapl/springrental/ArchetypeUnitTests.java
+++ src/test/java/com/mzapl/springrental/ArchetypeUnitTests.java
@@ -4,19 +4,17 @@ import com.mzapl.springrental.model.Archetype;
 import com.mzapl.springrental.model.Unit;
 import com.mzapl.springrental.service.ArchetypeService;
 import com.mzapl.springrental.service.UnitService;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.transaction.Transactional;
 
 @SpringBootTest
 @Transactional
-@RunWith(SpringRunner.class)
 public class ArchetypeUnitTests {
 
     @Autowired
@@ -24,32 +22,72 @@ public class ArchetypeUnitTests {
     @Autowired
     UnitService unitService;
 
-    static Archetype archetype;
-    static Unit unit;
+    Archetype archetype;
+    Unit unit;
+//
+//    @BeforeClass
+//    public static void init(){
+//        archetype = new Archetype();
+//        unit = new Unit(archetype);
+//    }
 
-    @BeforeClass
-    public static void init(){
+    @BeforeEach
+    public void init2(){
         archetype = new Archetype();
         unit = new Unit(archetype);
+        unit = unitService.save(unit);
+        archetype = archetypeService.save(archetype);
+
     }
 
+
     @Test
-    public void UnitHasArchetype(){
+    public void unitHasArchetype(){
         Assertions.assertEquals(archetype, unit.getArchetype());
     }
 
     @Test
-    public void ArchetypeHasUnit(){
-        archetypeService.save(archetype);
-        unitService.save(unit);
+    public void archetypeHasUnit(){
+//        init2();
+//        archetype = archetypeService.save(archetype);
+//        unit = unitService.save(unit);
         archetypeService.addUnit(unit);
         Assertions.assertTrue(archetype.getUnits().contains(unit));
     }
 
     @Test
+    public void unitAvailable(){
+        Assertions.assertTrue(unit.isAvailable());
+    }
+
+    @Test
+    public void archetypeUnitsNotEmpty(){
+//        unit = unitService.save(unit);
+        Assertions.assertTrue(archetype.getUnits().size() > 0);
+    }
+
+    @Test
+    public void findArchetypeByUnit(){
+        System.err.println(unitService.find(archetype));
+        Assertions.assertTrue(false);
+    }
+
+    @Test
     public void archetypeHasAvailableUnit(){
-        archetypeService.checkAvailability(archetype.getId());
+        archetype.setUnits(unitService.find(archetype));
+        archetypeService.checkAvailability(archetype);
         Assertions.assertTrue(archetype.getAvailable() > 0);
     }
 
+    @AfterEach
+    public void removeUnit(){
+        unitService.remove(unit.getId());
+        Assertions.assertFalse(unitService.find(unit.getId()).isPresent());
+    }
+
+    @AfterEach
+    public void removeArchetype(){
+        archetypeService.remove(archetype.getId());
+        Assertions.assertFalse(archetypeService.find(archetype.getId()).isPresent());
+    }
 }
